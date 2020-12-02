36ec5f686c9b948e85a0446ec7b62dec3d9bfba0
Bint Muhd <66806471+Bintmuhd@users.noreply.github.com>
2020-12-02 01:11:48 +0100

    update2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import { Box, Image, Text, Heading, SimpleGrid } from '@chakra-ui/react'
 import Typical from 'react-typical' 
-import {FaTwitter} from 'react-ic'
 
 
 
